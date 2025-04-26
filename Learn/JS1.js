a986f53999cefa36c59caab92a1396abf0e9aa41
B-skanda <120661341+B-skanda@users.noreply.github.com>
2025-04-26 15:46:17 +0000

    container config done

--- Learn/JS1.js
+++ Learn/JS1.js
@@ -1,1 +1,9 @@
-console.log("Hello! World Van");
+const accountId = 'AC1234'
+let accountEmail = 'noreply@gmail.com'
+var accountPassword = 'ghost@1234'
+
+// accountId = 'ACwerd' cannot re-assign the Const Variable.
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword]);
